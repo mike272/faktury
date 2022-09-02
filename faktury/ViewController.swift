7c0b418076afe992e1a76b249abc23c3d10ae169
mike272 <babinmichal@gmail.com>
2022-09-02 18:22:01 +0400

    Initial Commit

---
+++ faktury/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  faktury
+//
+//  Created by Wojciech Babinski on 02/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
